cff2c92e17ae61b695fa093db2bd1106acfe500e
Elsa McDowell <elmc2001@colorado.edu>
2024-07-23 15:30:22 +0000

    added db_create

--- app.py
+++ app.py
@@ -13,3 +13,19 @@ def testing():
     conn = psycopg2.connect("postgresql://emcdowell_db_user:TAAFXHs5Q6Zg07Xdoy8LJg197XOuh3DN@dpg-cqfsksdds78s73c50a6g-a/emcdowell_db")
     conn.close()
     return 'Database Connection Successful'
+
+@app.route('/db_create')
+def create():
+    conn = psycopg2.connect("postgresql://emcdowell_db_user:TAAFXHs5Q6Zg07Xdoy8LJg197XOuh3DN@dpg-cqfsksdds78s73c50a6g-a/emcdowell_db")
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS Basketball(
+        First varchar(255),
+        Last varchar(255),
+        City varchar(255),
+        Name varchar(255),
+        Number int
+        );
+    ''')
+    conn.commit()
+    conn.close()
+    return 'Basketball table created successfully'
